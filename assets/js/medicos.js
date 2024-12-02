74f0ff42bcd9c5629d3f6573ad0577d21d74757f
alangajardo <alan.gajardo@gmail.com>
2024-12-02 19:25:10 -0300

    se ordena el ejercicio sin funciones externas de consola

--- assets/js/medicos.js
+++ assets/js/medicos.js
@@ -1,3 +1,7 @@
+//Destructuring:
+//mostrar doctores disponibles:
+const contenedorMedicos = document.getElementById("equipoMedico");
+
 //función para obtener datos:
 function obtener_medicos(){
     return fetch("./assets/files/medicos.json")
@@ -38,69 +42,7 @@ function cargarDoctores(){
     });
 }
 
-//Destructuring:
-//mostrar doctores disponibles:
-const contenedorMedicos = document.getElementById("equipoMedico");
-
-function cargar_doctor_disponible(){
-    obtener_medicos().then(medicos =>{
-        console.warn("CLONACIÓN: 1ero ORIGINAL, 2do FILTRADO SIN AFECTAR ORIGINAL");
-        let medicos_clonados = JSON.parse(JSON.stringify(medicos))
-        medicos_clonados.forEach(m => {
-            m.disponibilidad = true;
-        });
-        console.log(medicos);
-        console.log(medicos_clonados);
-        
-        medicos.filter(m => m.disponibilidad).forEach(medico => {
-            console.warn("MÉDICOS FILTRADOS")
-            console.log(medico);
-            contenedorMedicos.appendChild( crear_card_medico(medico) );
-        });
-    })
-    .catch(error=>{
-        console.log("ERROR: " + error);
-    });
-}
-
-cargar_doctor_disponible();
-
-
-//implementación de estructuras de datos:
-function trabajar_lista_doctores(){
-    obtener_medicos().then(medicos => {
-        console.warn("TRABAJAR MEDICOS");
-        let nuevo_medico = {
-            "codigo_medico": 5,
-            "nombre": "Dr. Gregory House",
-            "especialidad": "General",
-            "descripcion": "El Dr. Housees genial.",
-            "contacto": {
-                "correo": null,
-                "telefono": null
-            },
-            "anios_exp": 10,
-            "disponibilidad": true
-        };
-        console.warn("ORIGINAL");
-        console.table(medicos);
-        
-        console.warn("AGREGADO NUEVO");
-        medicos.push(nuevo_medico);
-        console.table(medicos);
-        
-        console.warn("ELIMINAR DOCTOR");
-        let lista_actualizada = medicos.filter(m => m.codigo_medico != 3);
-        console.table(lista_actualizada);
-
-        console.warn("BUSCAR 1 DOCTOR");
-        console.log(medicos.find(m=>m.codigo_medico==2))
-    })
-    .catch(error=>{
-        console.log("ERROR: " + error);
-    });
-}
-trabajar_lista_doctores();
+cargarDoctores();
 
 //ORDENAR:
 function ordenar_menor_mayor(){
